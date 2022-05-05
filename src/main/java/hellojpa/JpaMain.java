bfd613cfd60fe661f12aa79c201bd98f104053de
hutswing <hutswing5613@gmail.com>
2022-05-05 23:11:57 +0900

    엔티티 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,13 +19,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-//
-            // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
-
-            System.out.println("===========");
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
